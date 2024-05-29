9a1caf310f3aa36967f87575acdb6b94286fc1fd
cvrcoe26 <guru02721@gmail.com>
2024-05-29 18:37:28 +0530

    Update cp.c

--- cp.c
+++ cp.c
@@ -3,8 +3,8 @@
 #include<unistd.h>
 int main(){
     char buff[30];
-    int inp =open("/home/cvr/test",D_RDONLY);
-    int op = open("/home/cvr/new",O_CREAT|O_RDWR,S_IRUSR|S_IWUSR|S_IRGRT|S_IROTH);
+    int inp =open("/home/cvr/test",O_RDONLY);
+    int op = open("/home/cvr/new",O_CREAT|O_RDWR,S_IRUSR |S_IWUSR |S_IRGRP |S_IROTH);
     while(read(inp,&buff,1)) 
         write(op,&buff,1);
     printf("File copied successfully!!");
